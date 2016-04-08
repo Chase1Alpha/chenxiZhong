0a9e025e374db47d1f9d8d1c35a81877271df892
Chase1Alpha <111798@gmail.com>
2016-04-08 17:17:28 -0400

    fix active tab

--- js/controllers/panel-controller.js
+++ js/controllers/panel-controller.js
@@ -1,13 +1,14 @@
 /**
  * Created by zhongchenxi on 3/31/16.
  */
-app.controller("PanelController", function(){
+angular.module("myResume")
+    .controller("PanelController", function(){
 
-    this.selectTab =function(setTab){
-        this.tab = setTab;
-    };
+        this.selectTab =function(setTab){
+            this.tab = setTab;
+        };
 
-    this.isSelected = function (checkTab){
-        return this.tab === checkTab;
-    };
+        this.isSelected = function (checkTab){
+            return this.tab === checkTab;
+        };
 });
